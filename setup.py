74579d8d9f1b03835c4876df2ed23c65a327f6e4
caltamiranda <92551465+caltamiranda@users.noreply.github.com>
2023-06-06 23:29:05 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='packet',
+    version='0.1',
+    description='trabajo pdm',
+    author='Carlos Altamiranda',
+    packages=find_packages(),
+    install_requires=[
+        # Lista de dependencias requeridas por tu paquete
+    ],
+)
