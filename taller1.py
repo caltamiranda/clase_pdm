ab2153f07987e2678e0f90abadb80b09c30996ff
caltamiranda <92551465+caltamiranda@users.noreply.github.com>
2023-06-07 10:02:06 -0500

    Update taller1.py

--- taller1.py
+++ taller1.py
@@ -1,1 +1,20 @@
+class Calculadora:
+    def _init_(self, n1, n2):
+        self.n1 = float(n1)
+        self.n2 = float(n2)
 
+    def sumar(self):
+        suma = self.n1 + self.n2
+        print("el resultado de la suma es: ", suma)
+
+    def restar(self):
+        resta = self.n1 - self.n2
+        print("el resultado de la resta es: ", resta)
+
+    def multiplicar(self):
+        multiplicacion = self.n1 * self.n2
+        print("el resultado de la multiplicación es: ", multiplicacion)
+
+    def dividir(self):
+        division = self.n1 / self.n2
+        print("el resultado de la división es: ", division)
